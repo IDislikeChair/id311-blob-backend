80fbb75abd40f6a92ae76f04db4fb81f7977536a
IDislikeChair <siripon.sutthiwanna@gmail.com>
2023-06-09 23:10:27 +0900

    Add missionOne sockets

--- mission/missionOne.js
+++ mission/missionOne.js
@@ -3,12 +3,26 @@ import { Emcee } from '../emcee.js';
 import { PlayerMgr } from '../playerMgr.js';
 
 export class MissionOne extends AbstractMission {
+  #step_counts;
+
   /**
    * @param {Emcee} emcee
    * @param {PlayerMgr} playerMgr
    */
   constructor(emcee, playerMgr) {
     super(emcee, playerMgr);
+
+    this.#step_counts = [0, 0, 0, 0, 0, 0];
+
+    this.playerMgr.on_any_alive_player('stepOn', (player_number, steps) => {
+      this.#step_counts[player_number] = steps;
+
+      this.emcee.emit('broadcastStepCounts', this.#step_counts);
+    });
+
+    this.emcee.on('resetStepCounts', () => {
+      this.#step_counts = [0, 0, 0, 0, 0, 0];
+    });
   }
 
   get_duration() {
@@ -16,26 +30,26 @@ export class MissionOne extends AbstractMission {
   }
 
   // this will be called after the duration above is expired.
+  // AKA When mission ends.
   run() {
-    this.emcee.emit('missionOne', 'success');
-    this.playerMgr.emit_to_all_players('message', 'Hello players');
-    this.playerMgr.emit_to_alive_players('message', 'Hello alive players');
-
-    this.emcee.on('message', (message) => {
-      console.log(message);
-    });
-    this.playerMgr.on_any_player('message', (message) => {
-      console.log(message);
-    });
-    this.playerMgr.on_any_alive_player('message', (message) => {
-      console.log(message);
-    });
-
-    this.playerMgr.emit_to_player(
-      0,
-      'message',
-      'Hello player 0. You will be dead'
-    );
-    this.playerMgr.set_player_dead(0);
+    // Example interface
+    // this.emcee.emit('missionOne', 'success');
+    // this.playerMgr.emit_to_all_players('message', 'Hello players');
+    // this.playerMgr.emit_to_alive_players('message', 'Hello alive players');
+    // this.emcee.on('message', (message) => {
+    //   console.log(message);
+    // });
+    // this.playerMgr.on_any_player('message', (message) => {
+    //   console.log(message);
+    // });
+    // this.playerMgr.on_any_alive_player('message', (message) => {
+    //   console.log(message);
+    // });
+    // this.playerMgr.emit_to_player(
+    //   0,
+    //   'message',
+    //   'Hello player 0. You will be dead'
+    // );
+    // this.playerMgr.set_player_dead(0);
   }
 }
