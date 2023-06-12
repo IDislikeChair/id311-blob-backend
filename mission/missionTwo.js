e47dc50fa702acccc13be2c6868cb9c1da062f9c
IDislikeChair <siripon.sutthiwanna@gmail.com>
2023-06-12 23:56:42 +0900

    Implement draft for Mission 2 backend

--- mission/missionTwo.js
+++ mission/missionTwo.js
@@ -1,13 +1,151 @@
 import { AbstractMission } from '../abstractMission.js';
 import { GameFlowMgr } from '../gameFlowMgr.js';
 
+class MissionTwoPair {
+  /**
+   * @param {number} solverNumber
+   * @param {number} guiderNumber
+   */
+  constructor(solverNumber, guiderNumber) {
+    this.solverNumber = solverNumber;
+    this.guiderNumber = guiderNumber;
+    this.score = 0;
+
+    this.answer = -1;
+    this.generate_answer();
+  }
+
+  generate_answer() {
+    // Generate integer from 0 to 4;
+    this.answer = Math.floor(Math.random() * 5);
+  }
+}
+
 export class MissionTwo extends AbstractMission {
+  /** @type {number[]} */
+  #alivePlayerNumbers;
+
+  /** @type {[MissionTwoPair, MissionTwoPair]} */
+  #pairs;
+
   /**
    * @param {GameFlowMgr} gameFlowMgr
    */
   constructor(gameFlowMgr) {
     super(gameFlowMgr);
+
+    // (1) Get the list of alive players from previous missions.
+    this.#alivePlayerNumbers = [];
+    for (let i = 0; i < 6; i++) {
+      if (this.playerMgr.is_player_alive(i)) {
+        this.#alivePlayerNumbers.push(i);
+      }
+    }
+
+    if (this.#alivePlayerNumbers.length !== 4) {
+      throw new Error('missionTwo: ');
+    }
+
+    // (2) Pair up players.
+    // TODO: Randomize the pairing up process.
+    this.#pairs[0] = new MissionTwoPair(
+      this.#alivePlayerNumbers[0],
+      this.#alivePlayerNumbers[1]
+    );
+    this.#pairs[1] = new MissionTwoPair(
+      this.#alivePlayerNumbers[2],
+      this.#alivePlayerNumbers[3]
+    );
+
+    // (2a) Send the score to TV.
+    this.#broadcastScoreToEmcee();
+
+    // (3) Initialize each player's role.
+    for (let pair of this.#pairs) {
+      this.playerMgr.emit_to_player(pair.solverNumber, 'missionTwoRole', 0);
+
+      this.playerMgr.emit_to_player(pair.guiderNumber, 'missionTwoRole', 1);
+
+      // Set up listener for when solver try answer.
+      // If the answer is correct, send signal to guider.
+      this.playerMgr.on_player(
+        pair.solverNumber,
+        'tryAnswer',
+        (/** @type {number} */ answer) => {
+          if (pair.answer === answer) {
+            this.playerMgr.emit_to_player(pair.guiderNumber, 'alertAnswer');
+          }
+        }
+      );
+
+      // Set up listener for when solver submit answer.
+      // Then, send status to both players.
+      // Always generate new answer even if fails.
+      this.playerMgr.on_player(
+        pair.solverNumber,
+        'submitAnswer',
+        (/** @type {number} */ answer) => {
+          if (pair.answer === answer) {
+            pair.score++;
+            this.#broadcastScoreToEmcee();
+          } else {
+            this.#broadcastFailureToEmcee(this.#pairs.indexOf(pair));
+          }
+
+          this.playerMgr.emit_to_player(
+            pair.solverNumber,
+            'isAnswerRight',
+            pair.answer === answer
+          );
+          this.playerMgr.emit_to_player(
+            pair.guiderNumber,
+            'isAnswerRight',
+            pair.answer === answer
+          );
+
+          pair.generate_answer();
+        }
+      );
+    }
   }
 
-  wrap_up() {}
+  #broadcastScoreToEmcee() {
+    this.emcee.emit('broadcastScore', {
+      pairOne: {
+        solverNumber: this.#pairs[0].solverNumber,
+        guiderNumber: this.#pairs[0].guiderNumber,
+        score: this.#pairs[0].score,
+      },
+      pairTwo: {
+        solverNumber: this.#pairs[1].solverNumber,
+        guiderNumber: this.#pairs[1].guiderNumber,
+        score: this.#pairs[1].score,
+      },
+    });
+  }
+
+  /**
+   * @param {any} pairNumber
+   */
+  #broadcastFailureToEmcee(pairNumber) {
+    this.emcee.emit('submitAnswerFail', pairNumber);
+  }
+
+  wrap_up() {
+    const winnerNumbers =
+      this.#pairs[0] > this.#pairs[1]
+        ? [this.#pairs[0].solverNumber, this.#pairs[0].guiderNumber]
+        : [this.#pairs[1].solverNumber, this.#pairs[1].guiderNumber];
+
+    // Set the rest of players dead.
+    for (let playerNumber = 0; playerNumber < 6; playerNumber++) {
+      if (!(playerNumber in winnerNumbers)) {
+        this.playerMgr.set_player_dead(playerNumber);
+      }
+    }
+
+    console.log(
+      `MissionTwo.wrap_up: Winners from this round are ${winnerNumbers}`
+    );
+  }
 }
